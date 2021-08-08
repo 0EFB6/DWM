69eb7af873ad7d5e7cd161cea590dabe47dda84e
0EFB6 <changws12345@gmail.com>
2021-08-08 21:47:37 +0800

    modified:   config.def.h

--- config.def.h
+++ config.def.h
@@ -6,8 +6,10 @@ static const unsigned int borderpx  = 0;        /* border pixel of windows */
 static const unsigned int snap      = 16;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=15", "fontawesome:size=15" };
-static const char dmenufont[]       = "monospace:size=15";
+static const char *fonts[]          = { "fontawesome:size=15" };
+/* "monospace:size=15" */
+static const char dmenufont[]       = "fontawesome:size=15";
+/* "monospace:size=15" */
 static const char col_gray1[]       = "#f9ea0e";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#01070C";
@@ -22,6 +24,10 @@ static const char *colors[][3]      = {
 /* tagging */
 /* static const char *tags[] = { "", "", "", "", "", "", "", "", "", "", "", "12"}; */
 static const char *tags[] = { "Skool", "    Dev     ", "    Fil    ", "    Oce   ", "   Msc   ", "  Wsp   ", "    Dd    ", "   Tty   ", "Misc "};
+
+/* For Arch Linux
+static const char *tags[] = { "Skool             ", "        Dev         ", "       Fil       ", "       Oce      ", "      Msc      ", "       Etc        ", "       Dd       ", "       Tty       ", "     Misc    "};
+*/
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -66,17 +72,19 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
-static const char *web[] = { "firefox-esr", NULL };
+static const char *web[] = { "firefox", NULL }; /* firefox-esr on Debian */
 static const char *file[] = { "pcmanfm", NULL };
 static const char *dd[] = { "discord", NULL };
 static const char *lock[] = { "slock", NULL };
-static const char *brave[] = { "brave-browser-stable", NULL };
+static const char *brave[] = { "brave-browser-stable", NULL }; /*brave on Arch */
 static const char *music[] = { "spotify", NULL };
-static const char *print_screen_cmd[] = { "import", "/home/linux/aaa.jpg", NULL};
+static const char *print_screen_cmd[] = { "scrot", NULL};
 static const char *screencapturing[] = { "obs", NULL };
 static const char *shutdown[] = { "/sbin/poweroff", NULL };
 static const char *restart[] = { "/sbin/reboot", NULL };
 static const char *audio[] = { "audio", NULL };
+static const char *windowcapture[] = { "winsct", NULL };
+static const char *rofi[] = { "rof", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -87,10 +95,12 @@ static Key keys[] = {
 	{ MODKEY,			XK_d,	   spawn,	   {.v = dd } },
 	{ MODKEY,			XK_o,	   spawn,	   {.v = screencapturing } },
 	{ 0,							XK_Print,  spawn,		   {.v = print_screen_cmd } },
+	{ MODKEY,						XK_Print,  spawn,		   {.v = windowcapture } },
 	{ MODKEY,						XK_l,	   spawn,		   {.v = lock } },
 	{ MODKEY,						XK_z,	   spawn,		   {.v = brave} },
 	{ MODKEY,						XK_s,	   spawn,		   {.v = music} },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
+	{ MODKEY,						XK_grave,  focusstack,	   {.i = +1 } },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY,			XK_q,	   spawn,	   {.v = audio } },
@@ -105,6 +115,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
+	{ MODKEY|ShiftMask,				XK_a,	   spawn,		   {.v = rofi } },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
